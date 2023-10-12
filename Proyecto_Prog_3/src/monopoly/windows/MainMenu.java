59b26517c2e288624cd1eabe2516e5eae5de4429
KaRLiToS3 <carlos.pena@opendeusto.es>
2023-10-12 11:29:52 +0200

    Main Menu buttons properly set

--- Proyecto_Prog_3/src/monopoly/windows/MainMenu.java
+++ Proyecto_Prog_3/src/monopoly/windows/MainMenu.java
@@ -1,7 +1,11 @@
 package monopoly.windows;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Rectangle;
 
 import javax.swing.*;
 import javax.swing.UIManager.LookAndFeelInfo;
@@ -34,6 +38,11 @@ public class MainMenu extends JFrame {
 		JPanel E = new JPanel();
 		JPanel S = new JPanel();
 		
+		C.setAlignmentX(Component.CENTER_ALIGNMENT);
+		C.setLayout(new BoxLayout(C, BoxLayout.Y_AXIS));
+		C.setAlignmentX(Component.CENTER_ALIGNMENT);
+		S.setLayout(new BoxLayout(S, BoxLayout.X_AXIS));
+		
 		add(N, BorderLayout.NORTH);
 		add(W, BorderLayout.WEST);
 		add(C, BorderLayout.CENTER);
@@ -42,6 +51,25 @@ public class MainMenu extends JFrame {
 		
 		JLabel title = new JLabel("MONOPOLY GAME");
 		title.setFont(mainT);
+		
+		JButton[] buttons = new JButton[8];
+		String[] bText = {"PLAY", "GAME SETTINGS", "USER ACHIEVEMENTS", "MATCH RECORD", "MANAGE USERS", "CREDITS", "HELP", "LEAVE GAME"};
+		Dimension bMenu = new Dimension(200,60);
+		for(int i = 0; i < 8; i++) {
+			buttons[i] = new JButton(bText[i]);
+			if(i <6) {
+				C.add(buttons[i]);
+				buttons[i].setAlignmentX(Component.CENTER_ALIGNMENT);
+				buttons[i].setPreferredSize(bMenu);
+				buttons[i].setMinimumSize(bMenu);
+				buttons[i].setMaximumSize(bMenu);
+				C.add(new Box.Filler(new Dimension(1, 1), null, null));
+			}else {
+				S.add(buttons[i]);
+				buttons[i].setAlignmentX(Component.LEFT_ALIGNMENT);
+				if(i == 6) S.add(Box.createHorizontalGlue());
+			}
+		}
 		
 		//ADDING THE COMPONENTS TO THE PANELS
 		N.add(title);

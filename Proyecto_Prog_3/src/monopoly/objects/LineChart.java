f3a47321341958c178bca847152e98c76f817ee0
KaRLiToS3 <carlos.pena@opendeusto.es>
2023-10-25 19:23:15 +0200

    Comments were added to the ListChart class

--- Proyecto_Prog_3/src/monopoly/objects/LineChart.java
+++ Proyecto_Prog_3/src/monopoly/objects/LineChart.java
@@ -18,22 +18,23 @@ public class LineChart {
 	private JFreeChart chart;
 	
 	public LineChart(String title, Match match) {
-		XYSeriesCollection dataset = new XYSeriesCollection();
-		chart = ChartFactory.createXYLineChart(title, "Currency", "Time", dataset );
+		
+		XYSeriesCollection dataset = new XYSeriesCollection();	//The data container
+		chart = ChartFactory.createXYLineChart(title, "Time", "Currency", dataset );		//Creator of the chart with the data
 		
 		for(int i = 0; i < match.getTurnCurrencyPerUser().keySet().size(); i++) {
 			User usr = match.getUsers().get(i);
 			TreeMap<Long, Integer> timeAndCurrencyData = match.getTurnCurrencyPerUser().get(usr);
 			
-			XYSeries leyend = new XYSeries(usr.getName());
+			XYSeries legend = new XYSeries(usr.getName());	//Assigns a legend to the graph of a user
 			
 			for(Long lng : timeAndCurrencyData.keySet()) {
-				leyend.add(convertToMinutes(lng), (double) timeAndCurrencyData.get(lng));
+				legend.add(convertToMinutes(lng), (double) timeAndCurrencyData.get(lng));	//Assigns points to the XY axles 
 			}
 			
-			dataset.addSeries(leyend);
-			XYPlot line = (XYPlot) chart.getPlot();
-			line.getRenderer().setSeriesStroke(i, new BasicStroke(5.0f));
+			dataset.addSeries(legend);	//The legend of every user and its data associated is added to the dataset
+			XYPlot line = (XYPlot) chart.getPlot();		//The Plot is the line that is drawn
+			line.getRenderer().setSeriesStroke(i, new BasicStroke(5.0f));		//This sets up the thickness of the line
 			
 		}		
 	}

169ada2166c66a5169fb76cd396285675d171032
xiker.goikoetxea <xiker.goikoetxea@E108L6.opendeusto.es>
2023-11-08 09:40:46 +0100

    Token position resize

--- Proyecto_Prog_3/src/monopoly/objects/Token.java
+++ Proyecto_Prog_3/src/monopoly/objects/Token.java
@@ -3,8 +3,10 @@ package monopoly.objects;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Insets;
 
 import javax.swing.JComponent;
+import javax.swing.JPanel;
 
 public class Token extends JComponent{
 	/**
@@ -14,12 +16,17 @@ public class Token extends JComponent{
 	
 	int x;
 	int y;
+	double perX;
+	double perY;
+	JPanel panel;
 	Color color;
+	Insets insets;
 	int cell;
 	
 	
 	static int counter = 0;
 	final static int radius = 10;
+	final static double perRadius = 0.0146198830409357;
 	final static int[] CELL_NUMBERS = {0,1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,34,35,36,37,38,39,40}; 
 	final static Color[] TOKEN_COLORS = {Color.RED, Color.ORANGE, Color.BLUE, Color.GREEN};
 	
@@ -38,6 +45,31 @@ public class Token extends JComponent{
 
 	public void setY(int y) {
 		this.y = y;
+	}
+	
+
+	public double getPerX() {
+		return perX;
+	}
+
+	public void setPerX(double perX) {
+		this.perX = perX;
+	}
+
+	public double getPerY() {
+		return perY;
+	}
+
+	public void setPerY(double perY) {
+		this.perY = perY;
+	}
+
+	public JPanel getPanel() {
+		return panel;
+	}
+
+	public void setPanel(JPanel panel) {
+		this.panel = panel;
 	}
 
 	public Color getColor() {
@@ -63,6 +95,17 @@ public class Token extends JComponent{
 		counter++;
 	}
 	
+	public Token(double perX, double perY, Color color, JPanel panel,Insets insets) {
+		this.perX = perX;
+		this.perY = perY;
+		this.color = color;
+		this.panel = panel;
+		this.insets = insets;
+		counter++;
+	}
+	
+	
+	
 	public Token() {
 		this (100*(counter+1),100*(counter+1),TOKEN_COLORS[counter]);
 	}
@@ -74,12 +117,16 @@ public class Token extends JComponent{
 		Graphics2D graphics2D = (Graphics2D)g;
 		
 		graphics2D.setPaint(color);
-		graphics2D.fillOval(x, y, radius*2, radius*2);
+//		graphics2D.fillOval(x, y, radius*2, radius*2);
+		double panelWidth = panel.getSize().getWidth();
+		int radiusCalc = (int)(perRadius*panelWidth);
+		graphics2D.fillOval((int)(perX*panelWidth-insets.left-radiusCalc), (int)(perY*panelWidth-insets.top-radiusCalc), radiusCalc*2, radiusCalc*2);
+	
 	}
 
 	@Override
 	public String toString() {
-		return "x = " + getX() + ", y = " + getY();
+		return "x = " + getX() + ", y = " + getY() + "perx = " + getPerX() + ", pery = " + getPerY();
 	}
 
 	
--- Proyecto_Prog_3/src/monopoly/windows/MainGameMenu.java
+++ Proyecto_Prog_3/src/monopoly/windows/MainGameMenu.java
@@ -15,7 +15,9 @@ import java.awt.event.MouseListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import java.awt.event.WindowAdapter;
@@ -41,9 +43,31 @@ private final URL boardPath = getClass().getResource("/monopoly/images/board_mon
 private final URL dicePath = getClass().getResource("/monopoly/images/dice.png");
 //	private static final String boardPath = "../images/board_monopoly.png";
 //	private static final String dicePath = "../images/dice.png";
-	private static final Point[] positionList = {new Point(599,626),new Point(543,643),new Point(487,631),new Point(434,645),new Point(377,635),new Point(322,637),new Point(267,649),new Point(212,638),new Point(162,653),new Point(109,644),new Point(18,622),new Point(21,568),new Point(22,514),new Point(24,461),new Point(22,405),new Point(21,351),new Point(24,295),new Point(27,239),new Point(25,184),new Point(28,137),new Point(32,57),new Point(107,52),new Point(162,59),new Point(216,56),new Point(269,55),new Point(323,60),new Point(379,60),new Point(433,62),new Point(484,59),new Point(543,58),new Point(599,52),new Point(616,134),new Point(614,186),new Point(597,239),new Point(618,291),new Point(600,349),new Point(596,405),new Point(620,458),new Point(618,565)};
+//	private static final Point[] positionList = {new Point(599,626),new Point(543,643),new Point(487,631),new Point(434,645),new Point(377,635),new Point(322,637),new Point(267,649),new Point(212,638),new Point(162,653),new Point(109,644),new Point(18,622),new Point(21,568),new Point(22,514),new Point(24,461),new Point(22,405),new Point(21,351),new Point(24,295),new Point(27,239),new Point(25,184),new Point(28,137),new Point(32,57),new Point(107,52),new Point(162,59),new Point(216,56),new Point(269,55),new Point(323,60),new Point(379,60),new Point(433,62),new Point(484,59),new Point(543,58),new Point(599,52),new Point(616,134),new Point(614,186),new Point(597,239),new Point(618,291),new Point(600,349),new Point(596,405),new Point(620,458),new Point(618,565)};
+	private static final double[] posdoubleX = {0.9167927382753404,0.8184568835098336,0.7367624810892587,0.028744326777609682,0.03479576399394856,0.030257186081694403,0.039334341906202726,0.16036308623298035,0.23903177004538578,0.9334341906202723,0.8986384266263238,};
+	private static final double[] posdoubleY = {0.9515885022692889,0.9757942511346445,0.9652042360060514,0.9440242057488654,0.8608169440242057,0.7776096822995462,0.08169440242057488,0.08018154311649017,0.08925869894099848,0.2087745839636914,0.3661119515885023,};
+	private static final List<double[]> posdoublelist = new ArrayList<>() {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+	{
+		add(new double[]{0.9167927382753404,0.9515885022692889});
+		add(new double[]{0.8184568835098336,0.9757942511346445});
+		add(new double[]{0.7367624810892587,0.9652042360060514});
+		add(new double[]{0.028744326777609682,0.9440242057488654});
+		add(new double[]{0.03479576399394856,0.8608169440242057});
+		add(new double[]{0.030257186081694403,0.7776096822995462});
+		add(new double[]{0.039334341906202726,0.08169440242057488});
+		add(new double[]{0.16036308623298035,0.08018154311649017});
+		add(new double[]{0.23903177004538578,0.08925869894099848});
+		add(new double[]{0.9334341906202723,0.2087745839636914});
+		add(new double[]{0.8986384266263238,0.3661119515885023});
+	}};
+	
 	// Token position setter
-//	private static List<Point> posList = new ArrayList<>();
+	private static List<Point> posList = new ArrayList<>();
 
 	
 	private List<Token> tokenList = new ArrayList<>();
@@ -55,7 +79,7 @@ private final URL dicePath = getClass().getResource("/monopoly/images/dice.png")
 		setMinimumSize(new Dimension(1000, 700));
 		setLocationRelativeTo(null);
 		setTitle("MONOPOLY");
-				
+
 		// PANEL FOR MAIN DISTRIBUTION
 		 
 		setLayout(new BorderLayout());
@@ -71,6 +95,8 @@ private final URL dicePath = getClass().getResource("/monopoly/images/dice.png")
 				for (Token token : tokenList) {
 					token.paintComponent(g);
 				}
+				
+				g.drawLine(0, 10, 400, 10);
 				repaint();
 			}
 		};
@@ -101,24 +127,43 @@ private final URL dicePath = getClass().getResource("/monopoly/images/dice.png")
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-//				tokenList.get(0).setX(tokenList.get(0).getX()+100);
-				// Token position setter
+//				 Token position setter
 //				String str = "{";
 //				for (Point p : posList) {
 //					str = str + "new Point("+(int)p.getX()+","+(int)p.getY()+"),";
+//					
 //				}
 //				str=str+"}";
-//			
 //				System.out.println(str);
+
+				
+//				String strX = "{";
+//				String strY = "{";
+//				for (Point p : posList) {
+//					strX = strX + (p.getX()/boardPanel.getSize().getWidth())+",";
+//					strY = strY + (p.getY()/boardPanel.getSize().getHeight())+",";
+//				}
+//				strX=strX+"}";
+//				strY=strY+"}";
+//			
+//				System.out.println(strX);
+//				System.out.println(strY);
+				
+//				String str="";
+//				for (int i = 0; i < posdoubleX.length; i++) {
+//					str = str + "add(new double[]{"+posdoubleX[i]+","+posdoubleY[i]+"});";
+//				}
+//				System.out.println(str);
+				System.out.println(getInsets());
+
 			}
 		});
 
 		addMouseListener( new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				// Token position setter
+//				 Token position setter
 //				posList.add(getMousePosition());
-				System.out.println(getMousePosition());
 			}
 		});
 
@@ -132,9 +177,14 @@ private final URL dicePath = getClass().getResource("/monopoly/images/dice.png")
 		setVisible(true);
 		
 		//try tokens	
+//		Insets insets = getInsets();
+//		for (Point p : positionList) {
+//			tokenList.add(new Token((int)p.getX()-insets.left, (int)p.getY()-insets.top, Color.RED));
+//		}
 		Insets insets = getInsets();
-		for (Point p : positionList) {
-			tokenList.add(new Token((int)p.getX()-insets.left, (int)p.getY()-insets.top, Color.RED));
+		for (double[] p : posdoublelist) {
+//			tokenList.add(new Token((int)(p[0]*boardPanel.getSize().getWidth()-insets.left-insets.right), (int)(p[1]*boardPanel.getSize().getWidth()-insets.top-insets.bottom), Color.RED));
+			tokenList.add(new Token(p[0], p[1], Color.RED,boardPanel,insets));
 		}
 	}
 	@Override

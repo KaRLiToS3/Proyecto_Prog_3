cd7dd3b303121e5a8d74921c413e5e1df0ede53f
KaRLiToS3 <carlos.pena@opendeusto.es>
2023-12-03 16:57:05 +0100

    *

--- Proyecto_Prog_3/src/monopoly/windows/CreditsMenu.java
+++ Proyecto_Prog_3/src/monopoly/windows/CreditsMenu.java
@@ -1,28 +1,13 @@
 package monopoly.windows;
 
-import java.awt.Desktop;
-import java.awt.FlowLayout;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.net.URI;
-import java.net.URL;
-
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
-import javax.swing.SwingUtilities;
 
 public class CreditsMenu extends MasterFrame {
 	private static final long serialVersionUID = 1L;
 
 	public CreditsMenu() {
-		setSize(400,440);
-		toBack();
-		setLocationRelativeTo(null);
-		setResizable(false);
+		setSize(1000,800);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setDefaultWindowIcon();
 		

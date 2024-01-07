df066ab149cfcd7797787752a99abf23a9a79891
David Falcón <f.falcon@opendeusto.es>
2024-01-07 17:56:02 +0100

    JUnitTest repaired

--- Proyecto_Prog_3/test/GraphFactoryTest.java
+++ Proyecto_Prog_3/test/GraphFactoryTest.java
@@ -21,7 +21,7 @@ public class GraphFactoryTest {
 		assertEquals("Juan", chart.getXYPlot().getDataset().getSeriesKey(1));
 		assertEquals("Damian", chart.getXYPlot().getDataset().getSeriesKey(2));
 		
-		//																int series ("Damian"), int item(x)
+		//int series ("Damian"), int item(x)
 		assertEquals(100, chart.getXYPlot().getDataset().getY(0, 0).doubleValue(), 0);
 		assertEquals(300, chart.getXYPlot().getDataset().getY(1, 1).doubleValue(), 0);
 		assertEquals(1000, chart.getXYPlot().getDataset().getY(2, 3).doubleValue(), 0);
